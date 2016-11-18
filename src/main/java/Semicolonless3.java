39b76da327585c274d9786381c215fe49ae6706c
yy_yank <yy.yank.dev.blog@gmail.com>
2016-11-18 23:58:47 +0900

    semicolonless reflection

--- src/main/java/Semicolonless3.java
+++ src/main/java/Semicolonless3.java
@@ -1,9 +1,10 @@
+
 /**
  * Created by yy_yank on 2016/11/15.
  */
 public class Semicolonless3 {
-    public static void main(String[] args) {
-        if (java.util.stream.Stream.of(Hello.values()).peek(SemicolonlessGreeter::greet).noneMatch(hage -> hage == null)) {
+    public static void main(String[] args) throws ClassNotFoundException, java.lang.reflect.InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        if (ClassLoader.getSystemClassLoader().loadClass(HelloSemicolonless.class.getName()).getMethod("main", String[].class).invoke(null, (Object)null) != null) {
         }
     }
 }
